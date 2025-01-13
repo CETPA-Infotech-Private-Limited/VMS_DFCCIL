311d2625e3b08fa2671864414b9bc1619d28a0f4
ranjeetkumar47 <rksingh6375@gmail.com>
2025-01-13 21:20:26 +0530

    remove unwanted code

--- src/assets/image/images.ts
+++ src/assets/image/images.ts
@@ -1,8 +1,6 @@
-import logo from './logo.png'
-import RedLogo from './RedLogo.png'
-import assetsManagement from './assets-management.png'
-import BlackLogo from './BlackLogo.png'
-import WhiteLogo from './DFC.png'
-import LoginIllustration from './36.png'
-import IndiaFlag from './Flag_of_India (1).svg'
-export { logo, assetsManagement, BlackLogo, RedLogo, WhiteLogo, LoginIllustration, IndiaFlag }
+import logo from './logo.png';
+import RedLogo from './RedLogo.png';
+import WhiteLogo from './DFC.png';
+import LoginIllustration from './36.png';
+import IndiaFlag from './Flag_of_India (1).svg';
+export { logo, RedLogo, WhiteLogo, LoginIllustration, IndiaFlag };
